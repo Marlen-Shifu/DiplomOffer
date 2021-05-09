19746bc043dbed30b016fc2e92d19b5a52c7ce3d
Marlen-Shifu <66721945+Marlen-Shifu@users.noreply.github.com>
2021-05-10 00:55:34 +0600

    Update settings.py

--- Diplom/settings.py
+++ Diplom/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^x$kzj-zg*n!dm2xaty(x1+=d5r36v1mbgxe)i55nprmz4r4-q'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
